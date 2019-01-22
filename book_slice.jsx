05ab3077b80c063da07fad774d1fa50feaea5ad6
szawel <p@kilku.com>
2019-01-22 19:53:23 +0100

    gently optimized

--- book_slice.jsx
+++ book_slice.jsx
@@ -1,8 +1,8 @@
-/* ///// Book slicesr v01 /////
+/* ///// Book slicer v01 /////
 it is a simple script for preparing graphics on the back of a book
 in order to work select grafick you whant to prepar
 avoid multi pages project files and more then 1 layer
-by kilku.com / Paweł Szarzyński
+by kilku.com / Pafnucy Szarzyński
 */
 
 var doc = app.activeDocument;
@@ -11,7 +11,7 @@ var objSelected = doc.selection;
 // UI to set number of slises
 // ==================================================
 nr = Number(prompt( "Ilosc składek = ", 9));
-var abOffset = Number(prompt("Ustaw artbord offset = ", 200));
+var abOffset = Number(prompt("Ustaw artbord offset = ", 10));
 
 
 // Expand all selected object one by one
@@ -21,63 +21,54 @@ for (i=0; i < objSelected.length; i++){
 }
 
 
-
 // re-select expand obj
 // create and re-name group
 // add objSelected (selected object ) to created group
 // ==================================================
-
 var objSelected = doc.selection;
 var workGroup = app.activeDocument.groupItems.add();
-workGroup.name = "nzawa_Grupy" + 1;
+workGroup.name = "nazwa_Grupy" + 1; // !!!!
 for (i=0; i < objSelected.length; i++){
-    objSelected[i].move( workGroup, ElementPlacement.INSIDE);
+    // objSelected[i].move( workGroup, ElementPlacement.INSIDE);
     objSelected[i].move( workGroup, ElementPlacement.PLACEATEND);
 }
 
 
-// var objSelected = doc.selection;
-
 // offset group to top left corner
 // ==================================================
-var mainGroup = workGroup.geometricBounds;
-workGroup.translate(mainGroup[0]*-1,mainGroup[1]*-1);
+var bboxGroup = workGroup.geometricBounds;
+workGroup.translate(bboxGroup[0]*-1,bboxGroup[1]*-1);
 
 
-var mainGroup = workGroup.geometricBounds;
 // add rectangle for cliping mask
-alert(workGroup.geometricBounds);
-// ================================================== !!!
-workGroup.pathItems.rectangle (0, 0, mainGroup[2]/nr, mainGroup[3]*-1);
-
+// ==================================================
+var bboxGroup = workGroup.geometricBounds; // !!!!
+workGroup.pathItems.rectangle(0, 0, bboxGroup[2]/nr, bboxGroup[3]*-1);
 
 
 // offset artboard to top left corner and resize to one slice size
 // ==================================================
-var mainGroup = workGroup.geometricBounds;
-var width = mainGroup[2];
-var height = mainGroup[3];
-var abBounds = doc.artboards[0].artboardRect;// left, top, right, bottom
-var firstAbx = abBounds[0] - abBounds[0];
-var firstAby = abBounds[1] - abBounds[1];
+var bboxGroup = workGroup.geometricBounds;
+var width = bboxGroup[2];
+var height = bboxGroup[3];
+var bboxAb = doc.artboards[0].artboardRect;// left, top, right, bottom
+var firstAbx = bboxAb[0] - bboxAb[0];
+var firstAby = bboxAb[1] - bboxAb[1];
 var firstAbw = width/nr;
 var firstAbh = height;
-
 
 doc.artboards[0].artboardRect = [firstAbx, firstAby, firstAbw, firstAbh];
 
 
-
 // Layers duplicate //
 // ==================================================
-var docRef = app.activeDocument;
-var workLayer = docRef.activeLayer ;
+var workLayer = doc.activeLayer ;
 workLayer.name = (nr-1).toString();
 
 for(i=0; i < nr-1; i++){
-    with(docRef) {
+    with(doc) {
 
-        var n=docRef.layers.length;
+        var n=doc.layers.length;
         x=workLayer.zOrderPosition;
         var ln=workLayer.name;
         var ol = layers.getByName(ln);
@@ -90,13 +81,9 @@ for(i=0; i < nr-1; i++){
             ol.pageItems[a].duplicate(nl, ElementPlacement.PLACEATBEGINNING);
             nl.visible = true;
         }
-        docRef.layers.getByName(i.toString()).move(docRef.layers[n-x], ElementPlacement.PLACEAFTER);
+        doc.layers.getByName(i.toString()).move(doc.layers[n-x], ElementPlacement.PLACEAFTER);
     }
 }
-
-
-
-
 
 
 // Arbord setup //
@@ -115,8 +102,7 @@ for(i=1; i < nr; i++){
     var newAB = doc.artboards.add(ABrect);
     doc.artboards[i].artboardRect = [ abPosx * ( i ), abPosy, abPosw * ( i + 1 ), abPosh ];
     newAB.name = "joint " + ( i + 1 );
-};
-
+}
 
 
 // arboard offset by abOffset value //
@@ -124,24 +110,18 @@ for(i=1; i < nr; i++){
 var artbord = doc.artboards;
 for( i=0; i <artbord.length; i++ ){
     doc.artboards[i].artboardRect = [doc.artboards[i].artboardRect[0] + ( abOffset * i),doc.artboards[i].artboardRect[1], doc.artboards[i].artboardRect[2] + ( abOffset * i),doc.artboards[i].artboardRect[3]];
-};
-
+}
 
 
 // layer offset by abOffset value //
 // ==================================================
-
-// for(i=0; i , )
-
 for(i=0; i < app.activeDocument.layers.length; i++){
      app.activeDocument.layers[i].groupItems[0].translate(abOffset * i, app.activeDocument.layers[0].groupItems[0].geometricBounds[1] );
 }
 
+MakeClipingMask();
 
-
-MakeclipingMask();
-
-var doc = app.activeDocument;
+// ajust cliping mask
 
 for(x=0; x < doc.layers.length; x++){
     var warstwa = doc.layers[x];
@@ -149,7 +129,6 @@ for(x=0; x < doc.layers.length; x++){
     warstwa.groupItems[0].pathItems[0].translate(firstAbw*x,0,true,true);
 
 }
-
 
 // ==================================================
 ///// Expand Funcion /////
@@ -160,27 +139,14 @@ function expandFile() {
 }
 
 // ==================================================
-    ///// Cliping Mask Funcion /////
-function MakeclipingMask(){
-    // if(app.documents.length < 1){
-    //     return;
-    // }
-    var doc = app.activeDocument;
+///// Clipin gmask Function /////
+function MakeClipingMask(){
+
     for(x=0; x < doc.layers.length; x++){
         var lr = doc.layers[x];
-        for(var i=0; i<lr.groupItems.length; i++){
-            var thisGroup = lr.groupItems[i];
-            doc.selection = null;
-            thisGroup.selected = true;
-            for(var j=0; j<doc.pageItems.length; j++){
-                var thisItem = doc.pageItems[j];
-                if(thisItem.selected){
-                    thisGroup.clipped = true;
-                    // thisItem.clipping = true;
-                    if(thisItem.typename == "PathItem" && thisItem.name == "proofLine"){
-                    }
-                }
-            }
-        }
+        var thisGroup = lr.groupItems[0];
+        // doc.selection = null;
+        thisGroup.selected = true;
+        thisGroup.clipped = true;
     }
 }
